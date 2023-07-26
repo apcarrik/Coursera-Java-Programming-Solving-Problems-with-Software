163db75983b0d8a8b2e02547f353abe0d5aa786f
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-07-26 11:33:47 -0700

    adding author and version on files

--- strings/StringsFirstAssignments/Part3.java
+++ strings/StringsFirstAssignments/Part3.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Part3 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author apcarrik
+ * @version 7/21/2023
  */
 public class Part3
 {
--- strings/StringsFirstAssignments/Part4.java
+++ strings/StringsFirstAssignments/Part4.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Part4 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author apcarrik
+ * @version 7/21/2023
  */
 
 import edu.duke.URLResource;
