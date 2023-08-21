fe86fa94c1079ff966e566ad7811a5d86057b4bb
Adam Carriker <apcarrik@alumni.ncsu.edu>
2023-08-20 17:35:12 -0700

    adding code for yearOfHighestRank, getAverageRank, and getTotalBirthsRankedHigher methods

--- csv/BabyNames.java
+++ csv/BabyNames.java
@@ -93,7 +93,8 @@ public class BabyNames
      * @param   fr2     FileResource representing file of births for year to find match
      * @param   name    String representing name to match
      * @param   gender  String representing gender to match
-     * @return          String representing the name from fr2 with the same rank as the given name in fr1.
+     * @return          String representing the name from fr2 with the same rank as the given name 
+     *                  in fr1.
      */
     public String whatIsNameInYear(FileResource fr1, FileResource fr2, String name, String gender)
     {
@@ -102,9 +103,92 @@ public class BabyNames
             return "NO NAME";
         }
         return getName(fr2, rank, gender);
+    }
+    
+    /**
+     * This method selects a range of files to process and returns an integer, the year with the 
+     * highest rank for the name and gender.
+     *
+     * @param   itr     File iterable representing files to check
+     * @param   name    String representing name to match
+     * @param   gender  String representing gender to match
+     * @return          int representing the year with the highest rank for the given name & gender
+     */
+    public int yearOfHighestRank(Iterable<File> itr, String name, String gender)
+    {
+        int highestRankYear = -1;
+        int highestRank = Integer.MAX_VALUE;
+        for (File f : itr) {
+            int thisyear = Integer.parseInt(f.getName().substring(3,7));
+            int thisrank = getRank(new FileResource(f), name, gender);
+            if (thisrank != -1 && thisrank < highestRank){
+                highestRankYear = thisyear;
+                highestRank = thisrank;
+            }
+        }        
+        return highestRankYear;
+    } 
+    
+    /**
+     * This method selects a range of files to process and returns a double representing the average
+     * rank of the name and gender over the selected files.
+     *
+     * @param   itr     File iterable representing files to check
+     * @param   name    String representing name to match
+     * @param   gender  String representing gender to match
+     * @return          double representing the average rank for given name and gender
+     */
+    public double getAverageRank(Iterable<File> itr, String name, String gender)
+    {
+        double avgRank = 0.0;
+        int numFiles = 0;
+        for (File f : itr) {
+            int thisrank = getRank(new FileResource(f), name, gender);
+            avgRank += thisrank;
+            numFiles++;
+        }
+        return avgRank / numFiles;
+    } 
+    
+    /**
+     * This method returns an integer, the total number of births of those names with the same 
+     * gender and same year who are ranked higher than name.
+     * 
+     * Note: if the year exists in files and the name does not exist for that gender in the file,
+     * it returns the total number of children born that year with the same gender.
+     *
+     * @param   year    int representing year to match
+     * @param   name    String representing name to match
+     * @param   gender  String representing gender to match
+     * @return          int representing the the total number of births
+     * 
+     */
+    public int getTotalBirthsRankedHigher(Iterable<File> itr, int year, String name, String gender)
+    {
+        int totalBirthsRankedHigher = 0;
+        for (File f : itr) {
+            int thisyear = Integer.parseInt(f.getName().substring(3,7));
+            FileResource fr = new FileResource(f);
+            if (thisyear == year) {
+                
+                for (CSVRecord rec: fr.getCSVParser(false)) {
+                    if (rec.get(1).equals(gender)) {
+                        if (rec.get(0).equals(name)) {
+                            return totalBirthsRankedHigher;
+                        } else {
+                            totalBirthsRankedHigher += Integer.parseInt(rec.get(2));
+                        }
+                    }
+                }
+                
+            }
+        }
+        return totalBirthsRankedHigher;
     } 
     
     
+    
+    // === Testing Methods ===
     
     /**
      * Tests the method totalBirths.
@@ -210,6 +294,90 @@ public class BabyNames
         System.out.println("actual: " + test );
         System.out.println("test passed? = " + (test.equals(expected)));  
     }
+
+    /**
+     * Tests the method yearOfHighestRank.
+     */    
+    public void testYearOfHighestRank() {
+        System.out.println("\n Testing yearOfHighestRank()");
+        System.out.println("\n With files: [\"yob2012short.csv\", \"yob2013short.csv\", \"yob2014short.csv\"]");
+        
+        DirectoryResource dr = new DirectoryResource();
+        Iterable<File> itr = dr.selectedFiles();
+        
+        int test = yearOfHighestRank(itr, "Mason", "M");
+        int expected = 2012;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (test == expected));   
+        
+        test = yearOfHighestRank(itr, "FooBarBaz", "M");
+        expected = -1;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (test == expected));   
+        
+    }
+
+    /**
+     * Tests the method getAverageRank.
+     */    
+    public void testGetAverageRank() {
+        System.out.println("\n Testing getAverageRank()");
+        System.out.println("\n With files: [\"yob2012short.csv\", \"yob2013short.csv\", \"yob2014short.csv\"]");
+        
+        DirectoryResource dr = new DirectoryResource();
+        Iterable<File> itr = dr.selectedFiles();
+        
+        double test = getAverageRank(itr, "Mason", "M");
+        double expected = 3.0;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (Math.abs(Math.abs(test)-Math.abs(expected)) < 0.00001));   
+        
+        test = getAverageRank(itr, "Jacob", "M");
+        expected = 2.6666666;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (Math.abs(Math.abs(test)-Math.abs(expected)) < 0.00001));
+        
+        test = getAverageRank(itr, "FooBarBaz", "M");
+        expected = -1.0;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (Math.abs(Math.abs(test)-Math.abs(expected)) < 0.00001));
+        
+    }
+
+    /**
+     * Tests the method getTotalBirthsRankedHigher.
+     */    
+    public void testGetTotalBirthsRankedHigher() {
+        System.out.println("\n Testing getTotalBirthsRankedHigher()");
+        System.out.println("\n With files: [\"yob2012short.csv\", \"yob2013short.csv\", \"yob2014short.csv\"]");
+        
+        DirectoryResource dr = new DirectoryResource();
+        Iterable<File> itr = dr.selectedFiles();
+        
+        int test = getTotalBirthsRankedHigher(itr, 2012, "Ethan", "M");
+        int expected = 15;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (test == expected));   
+        
+        test = getTotalBirthsRankedHigher(itr, 2010, "Ethan", "M");
+        expected = 0;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (test == expected));  
+        
+        test = getTotalBirthsRankedHigher(itr, 2013, "FooBarBaz", "M");
+        expected = 44;
+        System.out.println("expected: " + expected );
+        System.out.println("actual: " + test );
+        System.out.println("test passed? = " + (test == expected));   
+        
+    }
     
     public static void main (String[] args) {
         System.out.println("\n\n==== main ====");
@@ -218,5 +386,8 @@ public class BabyNames
         bn.testGetRank();
         bn.testGetName();
         bn.testWhatIsNameInYear();
+        bn.testYearOfHighestRank();
+        bn.testGetAverageRank();
+        bn.testGetTotalBirthsRankedHigher();
     }
 }
